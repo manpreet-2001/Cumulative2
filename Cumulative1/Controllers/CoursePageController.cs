31cdadf233dba1646a1cda3a44b1f4e581a0cc5f
Manpreet <manpreet123singh987@gmail.com>
2025-04-09 23:59:44 -0400

    Initial commit

---
+++ Cumulative1/Controllers/CoursePageController.cs
@@ -0,0 +1,56 @@
+﻿using Cumulative1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cumulative1.Controllers
+{
+    public class CoursePageController : Controller
+    {
+        private readonly CourseAPIController _api;
+
+        public CoursePageController(CourseAPIController api)
+        {
+            _api = api;
+        }
+
+        public IActionResult List()
+        {
+            List<Course> Courses = _api.ListCourse();
+            return View(Courses);
+        }
+        public IActionResult Show(int id)
+        {
+            Course SelectedCourse = _api.FindCourse(id);
+            return View(SelectedCourse);
+        }
+        [HttpGet]
+        public IActionResult New(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Course NewCourse)
+        {
+            int Id = _api.AddCourse(NewCourse);
+
+
+            return RedirectToAction("Show", new { id = Id });
+        }
+
+        [HttpGet]
+        public IActionResult DeleteConfirm(int id)
+        {
+            Course SelectedCourse = _api.FindCourse(id);
+            return View(SelectedCourse);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            int CId = _api.DeleteCourse(id);
+            // redirects to list action
+            return RedirectToAction("List");
+        }
+    }
+}
---
+++ Cumulative1/Controllers/StudentPageController.cs
@@ -0,0 +1,57 @@
+﻿using Cumulative1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cumulative1.Controllers
+{
+    public class StudentPageController : Controller
+    {
+        private readonly StudentAPIController _api;
+
+        public StudentPageController(StudentAPIController api)
+        {
+            _api = api;
+        }
+        public IActionResult List()
+        {
+            List<Student> Students = _api.ListStudent();
+            return View(Students);
+        }
+        public IActionResult Show(int id)
+        {
+            Student SelectedStudent = _api.FindStudent(id);
+            return View(SelectedStudent);
+        }
+        [HttpGet]
+        public IActionResult New(int id)
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(Student NewStudent)
+        {
+            int Id = _api.AddStudent(NewStudent);
+
+
+            return RedirectToAction("Show", new { id = Id });
+        }
+
+
+        [HttpGet]
+        public IActionResult DeleteConfirm(int id)
+        {
+            Student SelectedStudent = _api.FindStudent(id);
+            return View(SelectedStudent);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            int TeacherId = _api.DeleteStudent(id);
+            // redirects to list action
+            return RedirectToAction("List");
+        }
+
+    }
+}
